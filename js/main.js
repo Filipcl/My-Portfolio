3b2da92a350927433436330d0bd35e16e6e131f1
Filipcl <filip.larsen@live.no>
2019-08-23 22:05:52 +0200

    Updates

---
+++ js/main.js
@@ -0,0 +1,74 @@
+// Select DOM Items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+const icons = document.querySelectorAll(".icons");
+
+// Set Initial stat of menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+    //Set menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+    //Set menu state
+    showMenu = false;
+  }
+}
+
+// Detect request animation frame
+var scroll =
+  window.requestAnimationFrame ||
+  // IE Fallback
+  function(callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+var elementsToShow = document.querySelectorAll(".show-on-scroll");
+
+function loop() {
+  Array.prototype.forEach.call(elementsToShow, function(element) {
+    if (isElementInViewport(element)) {
+      element.classList.add("is-visible");
+    } else {
+      element.classList.remove("is-visible");
+    }
+  });
+
+  scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
